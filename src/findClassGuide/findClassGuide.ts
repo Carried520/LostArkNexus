364b9239dd7c888b334c33b1798ea50b1bd76c79
47chromosomes <ostrowski.cz@gmail.com>
2023-07-05 12:47:48 +0200

    Add Sheet Guide Nav and Change to Server Rerouting.

---
+++ src/findClassGuide/findClassGuide.ts
@@ -0,0 +1,4 @@
+import guides from './guides.json'
+export default function findClassGuide(className : string){
+    return guides.find(item => item.className == className || item.slugs.includes(className));
+}
