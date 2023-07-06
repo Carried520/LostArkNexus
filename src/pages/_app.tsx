206c701b86e6ecab1c7a9d284b8224113008b2eb
Poyoanon <{ID}+{username}@users.noreply.github.com>
2023-07-06 11:06:57 +0800

    Landing page redemption arc commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,45 +1,21 @@
-import React from 'react';
-import Head from 'next/head';
-import { AppProps } from 'next/app';
-import Page from '../app/page';
+import React from "react";
+import Head from "next/head";
+import type { AppProps } from "next/app";
+import "./global.css";
 
-const App = ({ Component, pageProps }: AppProps) => {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <React.Fragment>
       <Head>
-        <meta charSet="utf-8" />
-        <title>Bozo</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <style>
-          {`
-            @import url("https://fonts.googleapis.com/css?family=Lexend:600|Lexend+Zetta:400|Rubik+Mono+One:400|IBM+Plex+Sans+Condensed:600|Lexend+Deca:400");
-          `}
-        </style>
-        <style>
-          {`
-            @import url("https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css");
-            * {
-              -webkit-font-smoothing: antialiased;
-              box-sizing: border-box;
-            }
-            html,
-            body {
-              margin: 0px;
-              height: 100%;
-            }
-            a {
-              text-decoration: none;
-            }
-          `}
-        </style>
+        <title>locofy-react-nextjs-project</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
-      <body>
-        <div id="app">
-          <Page />
-        </div>
-      </body>
-    </>
+      <Component {...pageProps} />
+    </React.Fragment>
   );
-};
+}
 
-export default App;
+export default MyApp;
