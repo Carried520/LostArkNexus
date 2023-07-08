283af53a068a4622c487e76bd1034d3698a94163
47chromosomes <ostrowski.cz@gmail.com>
2023-07-09 00:32:19 +0200

    New destroyer alias and shock scrapper calc.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,11 +10,7 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
--- src/findClassGuide/findClassGuide.ts
+++ src/findClassGuide/findClassGuide.ts
@@ -1,6 +1,4 @@
 import guides from "./guides.json";
 export default function findClassGuide(className: string) {
-  return guides.find(
-    (item) => item.className.toLowerCase() == className.toLowerCase() || item.slugs.includes(className.toLowerCase())
-  );
+  return guides.find((item) => item.className.toLowerCase() == className.toLowerCase() || item.slugs.includes(className.toLowerCase()));
 }
