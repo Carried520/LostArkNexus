7ba3c5d169128bf974cf732b5f7d3f75a81ade9f
47chromosomes <ostrowski.cz@gmail.com>
2023-07-09 00:37:12 +0200

    Fix navigation

--- src/app/hub/page.tsx
+++ src/app/hub/page.tsx
@@ -1,7 +1,5 @@
-'use client'
-
-import { useRouter } from "next/navigation";
+import { redirect} from "next/navigation";
 export default function Home() {
-  const router = useRouter();
-  router.push('https://docs.google.com/document/d/1APWVyq77z2flMCc06_cqUnrNKee1U99p7R4XZe5FYV0/edit');
+
+  redirect('https://docs.google.com/document/d/1APWVyq77z2flMCc06_cqUnrNKee1U99p7R4XZe5FYV0/edit');
 }
