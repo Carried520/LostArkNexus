05a42a682f40834ada42b6a6fab309d2a0d20459
Poyoanon <{ID}+{username}@users.noreply.github.com>
2023-07-10 19:01:45 +0800

    Class selection components added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,27 @@
-'use client'
 import type { NextPage } from "next";
-import { useCallback, useEffect } from "react";
+import React, { useState } from "react";
 import styles from "./index.module.css";
 
-import HeroSection from "@/components/HeroSection"
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import ClassComponent from "@/components/ClassComponent";
 
-const App1: NextPage = () => {
-  useEffect(() => {
-    const scrollAnimElements = document.querySelectorAll(
-      "[data-animate-on-scroll]"
-    );
-    const observer = new IntersectionObserver(
-      (entries) => {
-        for (const entry of entries) {
-          if (entry.isIntersecting || entry.intersectionRatio > 0) {
-            const targetElement = entry.target;
-            targetElement.classList.add(styles.animate);
-            observer.unobserve(targetElement);
-          }
-        }
-      },
-      {
-        threshold: 0.15,
-      }
-    );
+interface AppProps {
+  selectedClass: string;
+}
 
-    for (let i = 0; i < scrollAnimElements.length; i++) {
-      observer.observe(scrollAnimElements[i]);
-    }
-
-    return () => {
-      for (let i = 0; i < scrollAnimElements.length; i++) {
-        observer.unobserve(scrollAnimElements[i]);
-      }
-    };
-  }, []);
-
-  const onGuidesClick = useCallback(() => {
-    const anchor = document.querySelector("[data-scroll-to='guides']");
-    if (anchor) {
-      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
-    }
-  }, []);
+const App: NextPage<AppProps> = () => {
+  const [selectedClass, setSelectedClass] = useState<string>('');
 
   return (
-    <>
+    <>     
+      <Navbar selectedClass={selectedClass} setSelectedClass={setSelectedClass} />
+      <div>
+        {selectedClass && <ClassComponent selectedClass={selectedClass} />}
+      </div>   
       <HeroSection />
     </>
   );
 };
 
-export default App1;
+export default App;
