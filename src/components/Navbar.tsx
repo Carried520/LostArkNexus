6af978e788362b3229208acdca65e312a7e50dac
47chromosomes <ostrowski.cz@gmail.com>
2023-07-11 13:49:08 +0200

    Fix ClassComponent dynamic object access Type Error and Add stronger types.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,8 +8,11 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ setSelectedClass, selectedClass }) => {
 
-  const handleButtonClick = (classItem: any) => {
-    setSelectedClass(classItem.name);
+  const handleButtonClick = (classItem : {name : string} | string) => {
+    if(typeof classItem == "string")
+      setSelectedClass(classItem)
+      else
+    setSelectedClass(classItem?.name);
   };
 
   const classArray = Object.values(classData);
@@ -18,7 +21,7 @@ const Navbar: React.FC<NavbarProps> = ({ setSelectedClass, selectedClass }) => {
     <nav>
       <button style={{color: 'white', marginRight: '10px'}} onClick={() => handleButtonClick('')}>Return to Home</button>
       <ul>
-        {classArray.map((classItem: any) => (
+        {classArray.map((classItem: {name : string}) => (
           <li key={classItem.name}>
             <button
               style={{ color: "white", marginRight: "10px" }}
