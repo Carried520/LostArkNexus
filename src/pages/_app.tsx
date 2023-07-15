1177c2d99a9cb5df3f39870c67065e0c23c2b319
47chromosomes <ostrowski.cz@gmail.com>
2023-07-16 01:57:01 +0200

    Fix for not working styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,7 +19,11 @@ export default function App({ Component, pageProps }: AppProps) {
           content="minimum-scale=1, initial-scale=1, width=device-width , user-scalable=no"
         />
       </Head>
+      <ChakraProvider>
+        <Navbar />
         <Component {...pageProps} />
+        <Footer />
+      </ChakraProvider>
     </React.Fragment>
   );
 }
