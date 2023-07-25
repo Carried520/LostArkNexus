1097ecfb9d09e87c4c9651b19dea2366a8de87a9
Poyoanon <27013504+Poyoanon@users.noreply.github.com>
2023-07-25 21:46:18 +0800

    Delete unused props

--- src/components/ClassComponent.tsx
+++ src/components/ClassComponent.tsx
@@ -14,9 +14,6 @@ const ClassComponent: React.FC<ClassComponentProps> = ({ selectedClass }) => {
     <div>
       <p style={{ color: "grey" }}>Name: {name}</p>
       <p style={{ color: "grey" }}>Description: {description}</p>
-      <p style={{ color: "grey" }}>Role: {role}</p>
-      <p style={{ color: "grey" }}>Difficulty: {difficulty}</p>
-      <p style={{ color: "grey" }}>Range: {range}</p>
     </div>
   );
 };
