447e0461a4cec4a2beebef98a306a8694224614c
47chromosomes <ostrowski.cz@gmail.com>
2023-08-18 00:43:08 +0200

    Change scrapper link and calculator description

--- src/Store/RandomClassStore.ts
+++ src/Store/RandomClassStore.ts
@@ -1,24 +1,22 @@
-import { create } from 'zustand'
-import classData from "../data/classData.json"
+import { create } from "zustand";
+import classData from "../data/classData.json";
 
 const classArray = Object.values(classData);
 const getRandomClass = () => {
-  
-    return classArray[Math.floor(Math.random() * classArray.length)]
-}
+  return classArray[Math.floor(Math.random() * classArray.length)];
+};
 
-type RandomClass = typeof classArray[number];
-
+type RandomClass = (typeof classArray)[number];
 
 type RandomClassState = {
-    randomClass : RandomClass
-}
+  randomClass: RandomClass;
+};
 
 type RandomClassAction = {
-    updateClass : (state : RandomClassState) => void
-}
+  updateClass: (state: RandomClassState) => void;
+};
 
 export const useRandomClass = create<RandomClassState & RandomClassAction>((set) => ({
   randomClass: getRandomClass(),
-  updateClass: () => set(() => ({randomClass : getRandomClass()})),
-}))
+  updateClass: () => set(() => ({ randomClass: getRandomClass() })),
+}));
