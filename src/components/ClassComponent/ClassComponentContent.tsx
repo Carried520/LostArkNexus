7288bbb1254770ae3ae562de0e6bf4bab47db8cb
Haste <haste.stream@gmail.com>
2023-08-29 16:06:52 -0400

    [dev-raid-guides-navbar] code cleanup, prettier, icons

--- src/components/ClassComponent/ClassComponentContent.tsx
+++ src/components/ClassComponent/ClassComponentContent.tsx
@@ -1,60 +1,73 @@
 import classData from "../../data/classData.json";
-import { Separator } from "@/components/ui/separator"
-import { FaLink } from 'react-icons/fa'
-import Link from 'next/link';
+import { Separator } from "@/components/ui/separator";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { FaLink } from "react-icons/fa";
 
 type ContentProps = {
-    selectedClass : string
-}
+  selectedClass: string;
+};
 
-
-
-export default function ClassComponentContent({selectedClass} : ContentProps){
-    if (!(selectedClass in classData)) return;
+export default function ClassComponentContent({ selectedClass }: ContentProps) {
+  if (!(selectedClass in classData)) return;
   const selectedKey = selectedClass as keyof typeof classData;
   const { name, description, icon, color, links } = classData[selectedKey];
-    return <div className="grid grid-cols-3 h-screen">
-    <div className="sm:col-span-2 2xs:col-span-3 md relative">
-      <div className="lg:h-1/3 sm:h-1/5 flex flex-col justify-end items-center">
-        <p className="text-white text-xl font-classTitle uppercase lg:text-6xl md:text-5xl sm:text-4xl xs:text-4xl">
-          {name}
-        </p>
-      </div>
-      <div className="bg-classbg h-full flex flex-col py-5">
-        <Separator className="w-4/5 bg-gray-700 place-self-center"/>
-            <p className="text-white text-left xl:text-base 2xl:text-3xl font-classSubheader pt-2 2xl:mx-36 xl:mx-28 sm:mx-16 2xs:mx-8 uppercase">
-              Introduction
-            </p>
-            <p className="text-white lg:text-sm sm:text-xs 2xs:text-2xs text-left font-body pt-0.5 pb-2 2xl:mx-36 xl:mx-28 sm:mx-16 2xs:mx-8">
-              {description}
-            </p>
-        <Separator className="w-4/5 bg-gray-700 place-self-center" />
-        <div className="grid lg:grid-cols-3 sm:grid-cols-2 md:gap-3 2xs:gap-2 my-5 relative font-body 2xs:mx-2 sm:mx-auto">
-          {links?.map((linkItem, index) => (
-            <React.Fragment key={index}>
-              <div className="2xs:border-r-0 2xs:border-b-2 sm:border-b-0 sm:border-r-2 2xs:border-gray-700 px-4 2xs:mx-auto sm:ml-auto sm:mr-0 flex items-center md:text-center">
-                <p className="text-white xl:text-sm 2xs:text-xs text-center hover:underline font-medium m-auto flex items-center">
-                  <FaLink className="mr-2" />
-                  <Link href={linkItem.link} target="_blank">{linkItem.linkname}</Link>
-                  
-                </p>
-              </div>
-              <div className="lg:col-span-2 lg:mr-30 sm:mr-auto sm:ml-0 2xs:mx-auto pl-2">
-                <p className="text-white xl:text-sm md:text-xs 2xs:text-2xs font-thin">{linkItem.linkdescription}</p>
-              </div>
-            </React.Fragment>
-          ))}
+  return (
+    <div className="grid h-screen grid-cols-3">
+      <div className="md relative 2xs:col-span-3 sm:col-span-2">
+        <div className="flex flex-col items-center justify-end sm:h-1/5 lg:h-1/3">
+          <p className="font-classTitle text-xl uppercase text-white xs:text-4xl sm:text-4xl md:text-5xl lg:text-6xl">
+            {name}
+          </p>
+        </div>
+        <div className="flex h-full flex-col bg-classbg py-5">
+          <Separator className="w-4/5 place-self-center bg-gray-700" />
+          <p className="pt-2 text-left font-classSubheader uppercase text-white 2xs:mx-8 sm:mx-16 xl:mx-28 xl:text-base 2xl:mx-36 2xl:text-3xl">
+            Introduction
+          </p>
+          <p className="pb-2 pt-0.5 text-left font-body text-white 2xs:mx-8 2xs:text-2xs sm:mx-16 sm:text-xs lg:text-sm xl:mx-28 2xl:mx-36">
+            {description}
+          </p>
+          <Separator className="w-4/5 place-self-center bg-gray-700" />
+          <div className="relative my-5 grid font-body 2xs:mx-2 2xs:gap-2 sm:mx-auto sm:grid-cols-2 md:gap-3 lg:grid-cols-3">
+            {links?.map((linkItem, index) => (
+              <React.Fragment key={index}>
+                <div className="flex items-center px-4 2xs:mx-auto 2xs:border-b-2 2xs:border-r-0 2xs:border-gray-700 sm:ml-auto sm:mr-0 sm:border-b-0 sm:border-r-2 md:text-center">
+                  <p className="m-auto flex items-center text-center font-medium text-white hover:underline 2xs:text-xs xl:text-sm">
+                    <FaLink className="mr-2" />
+                    <Link href={linkItem.link} target="_blank">
+                      {linkItem.linkname}
+                    </Link>
+                  </p>
+                </div>
+                <div className="lg:mr-30 pl-2 2xs:mx-auto sm:ml-0 sm:mr-auto lg:col-span-2">
+                  <p className="font-thin text-white 2xs:text-2xs md:text-xs xl:text-sm">
+                    {linkItem.linkdescription}
+                  </p>
+                </div>
+              </React.Fragment>
+            ))}
+          </div>
         </div>
       </div>
+
+      <div className="relative 2xs:hidden sm:block">
+        <Image
+          loading="eager"
+          priority
+          fill
+          className="absolute inset-0 right-10 z-10 h-5/6 w-5/6 object-contain md:translate-x-12 lg:translate-x-14 xl:translate-x-20"
+          src={icon}
+          alt=""
+        />
+        <div className="xs:h-fit sm:h-1/6 md:h-1/5 lg:h-1/3"></div>
+        <div
+          className="-z-20 h-screen w-screen transform xs:translate-x-20 xs:skew-x-0 sm:translate-x-20 sm:skew-x-12 xl:translate-x-20"
+          style={{ backgroundColor: color }}
+        ></div>
+        <div className="absolute bottom-0 -z-10 w-screen bg-classbg xs:h-full md:h-4/5 lg:h-2/3"></div>
+      </div>
     </div>
-    
-    <div className="relative 2xs:hidden sm:block">
-      <Image loading="eager" priority fill className="absolute h-5/6 w-5/6 object-contain z-10 inset-0 right-10 xl:translate-x-20 lg:translate-x-14 md:translate-x-12" src={icon} alt="" />
-      <div className="lg:h-1/3 md:h-1/5 sm:h-1/6 xs:h-fit"></div>
-      <div className="sm:skew-x-12 xs:skew-x-0 h-screen w-screen -z-20 transform xs:translate-x-20 sm:translate-x-20 xl:translate-x-20" style={{backgroundColor: color}}></div>
-      <div className="absolute bottom-0 bg-classbg w-screen lg:h-2/3 md:h-4/5 xs:h-full -z-10"></div>
-    </div>
-  </div>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,13 @@
 "use client";
-import React from "react";
-import Link from "next/link";
+
 import classData from "../data/classData.json";
 import raidData from "../data/raidData.json";
-import { cn } from "@/lib/utils";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -12,10 +16,11 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
-import {useRouter } from "next/navigation";
-
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 const Navbar: React.FC = () => {
   const router = useRouter();
@@ -35,107 +40,187 @@ const Navbar: React.FC = () => {
 
   const raidArray = Object.values(raidData);
   const categoryRaidMap = raidArray.reduce((map, raidItem) => {
-  const { raid } = raidItem;
-  if (!map.has(raid)) {
-    map.set(raid, [raidItem]);
-  } else {
-    map.get(raid).push(raidItem);
-  }
-  return map;
-}, new Map());
-
+    const { raid } = raidItem;
+    if (!map.has(raid)) {
+      map.set(raid, [raidItem]);
+    } else {
+      map.get(raid).push(raidItem);
+    }
+    return map;
+  }, new Map());
 
   return (
-    <NavigationMenu className="ml-8 my-6">
-      <NavigationMenuList  className="space-x-3">
+    <NavigationMenu className="my-6 ml-8">
+      <NavigationMenuList className="space-x-3">
         <NavigationMenuItem className="relative">
           <Image
             sizes="(min-width: 1200px) 100%"
             priority
             fill
-            className={cn("cursor-pointer object-cover relative")}
+            className={cn("relative cursor-pointer object-cover")}
             src="/loa-nav.png"
             alt="Home"
-            onClick={() => router.push('/')}
+            onClick={() => router.push("/")}
           />
         </NavigationMenuItem>
-        <NavigationMenuItem  onMouseEnter={() => setMenuOpen(true)} onTouchStart={() => setMenuOpen(!menuOpen)}>
-          <NavigationMenuTrigger onPointerMove={(e) => e.preventDefault()} onPointerLeave={(e) => e.preventDefault()} className="text-foreground font-cla  ssSubheader text-lg">Class Guides</NavigationMenuTrigger>
+        <NavigationMenuItem
+          onMouseEnter={() => setMenuOpen(true)}
+          onTouchStart={() => setMenuOpen(!menuOpen)}
+        >
+          <NavigationMenuTrigger
+            onPointerMove={(e) => e.preventDefault()}
+            onPointerLeave={(e) => e.preventDefault()}
+            className="font-cla ssSubheader  text-lg text-foreground"
+          >
+            Class Guides
+          </NavigationMenuTrigger>
           {menuOpen && (
-            <NavigationMenuContent onPointerMove={(e) => e.preventDefault()} onPointerLeave={(e) => e.preventDefault()}  data-state={menuOpen}>
+            <NavigationMenuContent
+              onPointerMove={(e) => e.preventDefault()}
+              onPointerLeave={(e) => e.preventDefault()}
+              data-state={menuOpen}
+            >
               <ul className="grid w-[300px] gap-3 p-4 md:w-[300px] lg:w-[300px]">
-                {Array.from(categoryClassMap.entries()).map(([category, filteredClasses]) => (
-                  <Accordion key={category} type="single" collapsible>
-                    <AccordionItem value={category} className="text-sm leading-tight text-foreground">
-                      <AccordionTrigger>{category}</AccordionTrigger>
-                      <AccordionContent>
-                        {filteredClasses.map((filteredClassItem: { name: string }, index: number) => (
-                          <p
-                            key={index.toString()}
-                            className={cn("cursor-pointer hover:underline")}
-                            onClick={() => {
-                              router.push(`/guides/${filteredClassItem.name}`)
-                              setMenuOpen(false);
-                            }}
-                            onTouchEnd={() => {
-                              router.push(`/guides/${filteredClassItem.name}`)
-                              setMenuOpen(false);
-                            }}
-                          >
-                            {filteredClassItem.name}
-                          </p>
-                        ))}
-                      </AccordionContent>
-                    </AccordionItem>
-                  </Accordion>
-                ))}
+                {Array.from(categoryClassMap.entries()).map(
+                  ([category, filteredClasses]) => (
+                    <Accordion key={category} type="single" collapsible>
+                      <AccordionItem
+                        value={category}
+                        className="text-sm leading-tight text-foreground"
+                      >
+                        <AccordionTrigger>{category}</AccordionTrigger>
+                        <AccordionContent>
+                          {filteredClasses.map(
+                            (
+                              filteredClassItem: {
+                                name: string;
+                                icon: string;
+                              },
+                              index: number
+                            ) => (
+                              <div
+                                key={index.toString()}
+                                className="mb-2 flex items-center overflow-hidden"
+                              >
+                                <div className="relative mr-1 flex h-5 w-5 items-center justify-center overflow-hidden">
+                                  <Image
+                                    fill
+                                    object-objectFit="cover"
+                                    src={filteredClassItem.icon}
+                                    alt={filteredClassItem.name}
+                                  />
+                                </div>
+                                <p
+                                  className="cursor-pointer hover:underline"
+                                  onClick={() => {
+                                    router.push(
+                                      `/guides/${filteredClassItem.name}`
+                                    );
+                                    setMenuOpen(false);
+                                  }}
+                                  onTouchEnd={() => {
+                                    router.push(
+                                      `/guides/${filteredClassItem.name}`
+                                    );
+                                    setMenuOpen(false);
+                                  }}
+                                >
+                                  {filteredClassItem.name}
+                                </p>
+                              </div>
+                            )
+                          )}
+                        </AccordionContent>
+                      </AccordionItem>
+                    </Accordion>
+                  )
+                )}
               </ul>
             </NavigationMenuContent>
           )}
         </NavigationMenuItem>
-        <NavigationMenuItem onMouseEnter={() => setMenuOpen(true)} onTouchStart={() => setMenuOpen(!menuOpen)}>
-          <NavigationMenuTrigger onPointerMove={(e) => e.preventDefault()} onPointerLeave={(e) => e.preventDefault()} className="text-foreground font-cla  ssSubheader text-lg">Raid Guides</NavigationMenuTrigger>
+        <NavigationMenuItem
+          onMouseEnter={() => setMenuOpen(true)}
+          onTouchStart={() => setMenuOpen(!menuOpen)}
+        >
+          <NavigationMenuTrigger
+            onPointerMove={(e) => e.preventDefault()}
+            onPointerLeave={(e) => e.preventDefault()}
+            className="font-cla ssSubheader  text-lg text-foreground"
+          >
+            Raid Guides
+          </NavigationMenuTrigger>
           {menuOpen && (
-            <NavigationMenuContent onPointerMove={(e) => e.preventDefault()} onPointerLeave={(e) => e.preventDefault()}  data-state={menuOpen}>
+            <NavigationMenuContent
+              style={{ left: "200px" }}
+              onPointerMove={(e) => e.preventDefault()}
+              onPointerLeave={(e) => e.preventDefault()}
+              data-state={menuOpen}
+            >
               <ul className="grid w-[300px] gap-3 p-4 md:w-[300px] lg:w-[300px]">
-                {Array.from(categoryRaidMap.entries()).map(([category, filteredRaids]) => (
-                  <Accordion key={category} type="single" collapsible>
-                    <AccordionItem value={category} className="text-sm leading-tight text-foreground">
-                      <AccordionTrigger>{category}</AccordionTrigger>
-                      <AccordionContent>
-                        {filteredRaids.map((filteredRaidItem: { raid: string, links: { link: string, linkname: string }[] }, index: number) => (
-                          <div key={index.toString()}>
-                            {filteredRaidItem.links.map((linkItem: { link: string, linkname: string }, linkIndex: number) => (
-                              <p
-                                key={linkIndex.toString()}
-                                className={cn("cursor-pointer hover:underline")}
-                                onClick={() => {
-                                router.push(linkItem.link)
-                                setMenuOpen(false);
-                            }}
-                          onTouchEnd={() => {
-                          router.push(linkItem.link)
-                          setMenuOpen(false);
-                        }}>
-                      {linkItem.linkname}
-                    </p>
-                  ))}
-                </div>
-              ))}
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-      ))}
-    </ul>
-  </NavigationMenuContent>
-)}
-
-
-
-
+                {Array.from(categoryRaidMap.entries()).map(
+                  ([category, filteredRaids]) => (
+                    <Accordion key={category} type="single" collapsible>
+                      <AccordionItem
+                        value={category}
+                        className="text-sm leading-tight text-foreground"
+                      >
+                        <AccordionTrigger>{category}</AccordionTrigger>
+                        <AccordionContent>
+                          {filteredRaids.map(
+                            (
+                              filteredRaidItem: {
+                                raid: string;
+                                links: {
+                                  link: string;
+                                  linkname: string;
+                                }[];
+                              },
+                              index: number
+                            ) => (
+                              <div key={index.toString()}>
+                                {filteredRaidItem.links.map(
+                                  (
+                                    linkItem: {
+                                      link: string;
+                                      linkname: string;
+                                    },
+                                    linkIndex: number
+                                  ) => (
+                                    <p
+                                      key={linkIndex.toString()}
+                                      className={cn(
+                                        "cursor-pointer hover:underline"
+                                      )}
+                                      onClick={() => {
+                                        router.push(linkItem.link);
+                                        setMenuOpen(false);
+                                      }}
+                                      onTouchEnd={() => {
+                                        router.push(linkItem.link);
+                                        setMenuOpen(false);
+                                      }}
+                                    >
+                                      {linkItem.linkname}
+                                    </p>
+                                  )
+                                )}
+                              </div>
+                            )
+                          )}
+                        </AccordionContent>
+                      </AccordionItem>
+                    </Accordion>
+                  )
+                )}
+              </ul>
+            </NavigationMenuContent>
+          )}
         </NavigationMenuItem>
 
-        <NavigationMenuItem  className={`text-foreground font-classSubheader text-lg ${navigationMenuTriggerStyle()}`}>
+        <NavigationMenuItem
+          className={`font-classSubheader text-lg text-foreground ${navigationMenuTriggerStyle()}`}
+        >
           <Link href="/lfg">LFG</Link>
         </NavigationMenuItem>
       </NavigationMenuList>
