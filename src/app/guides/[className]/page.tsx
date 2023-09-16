af6b27f2400ab56f4f0ff1d2108b9886b22a4790
Haste <haste.stream@gmail.com>
2023-09-16 19:20:14 -0400

    [feature/seo-updates] update seo + add og image

--- src/app/guides/[className]/page.tsx
+++ src/app/guides/[className]/page.tsx
@@ -1,6 +1,22 @@
 import ClassComponent from "@/components/ClassComponent/ClassComponent";
+import { getClassInfo } from "@/components/ClassComponent/ClassComponentContent";
+import { ResolvingMetadata, Metadata } from "next";
 
-export default function Page({ params }: { params: { className : string } }){
-    const capitalizedClass = params.className[0].toUpperCase() + params.className.slice(1);
-    return <ClassComponent selectedClass={capitalizedClass}/>
+export async function generateMetadata({
+  params,
+}: {
+  params: { className: string };
+}): Promise<Metadata> {
+  const { name, description, color } = getClassInfo(params.className);
+  return {
+    title: `Lost Ark Nexus | ${name} Guide`,
+    description: description,
+    themeColor: color,
+  };
+}
+
+export default function Page({ params }: { params: { className: string } }) {
+  const capitalizedClass =
+    params.className[0].toUpperCase() + params.className.slice(1);
+  return <ClassComponent selectedClass={capitalizedClass} />;
 }
