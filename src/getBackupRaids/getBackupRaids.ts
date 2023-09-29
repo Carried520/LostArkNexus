7e037bdf7a23cc04b9197b75d03d4804985a30a9
47chromosomes <ostrowski.cz@gmail.com>
2023-09-29 22:26:49 +0200

    Another serverless fix attempt

--- src/getBackupRaids/getBackupRaids.ts
+++ src/getBackupRaids/getBackupRaids.ts
@@ -1,10 +1,14 @@
 import path from "path";
 import fs from 'fs/promises'
 
-export default async function getBackupGuides(){
+async function getSnapShotDir(){
     const destinationDir = "snapshots";
-    const dir = path.join("./public/", destinationDir);
-    const filesInFolder = await fs.readdir(dir);
+    const dir = path.resolve("./public/", destinationDir);
+    return dir;
+}
+export default async function getBackupGuides(){
+    
+    const filesInFolder = await fs.readdir(await getSnapShotDir());
     const recordOfSnapshots = filesInFolder.reduce((acc : Record<string,string> , item) => {
         const fileName = path.parse(item).name;
         const indexer = fileName.slice(0 , fileName.indexOf(" ")).toLowerCase();
